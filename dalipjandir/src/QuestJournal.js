14fabb84fa0c4e4afb3f1afd88fb958b213dfe71
jandird <jandird@mcmaster.ca>
2019-07-05 01:11:56 -0400

    Added card component

--- dalipjandir/src/QuestJournal.js
+++ dalipjandir/src/QuestJournal.js
@@ -6,7 +6,27 @@ function QuestJournal() {
 
     return (
         <div id="quest-journal">
-            <p>Hello World</p>
+            <div className="wrapper">
+                <Card id="c0"/>
+                <Card id="c1"/>
+                <Card id="c2"/>
+            </div>
+        </div>
+    );
+}
+
+function Card (props) {
+
+    return (
+        <div className="card" id={props.id}>
+            <div className="story">
+                <div className="info">
+                    <h3>Pyramids</h3>
+                    <p> Built during a time when Egypt was one of the richest and most powerful civilizations in
+                        the world. Their massive scale reflects the unique role that the pharaoh played in
+                        ancient Egyptian society.</p>
+                </div>
+            </div>
         </div>
     );
 }
