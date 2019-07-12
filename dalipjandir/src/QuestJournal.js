f592fb734e9a3d69e93d61cb1fec3eb2ddbf44db
jandird <jandird@mcmaster.ca>
2019-07-12 00:25:31 -0400

    Added project card

--- dalipjandir/src/QuestJournal.js
+++ dalipjandir/src/QuestJournal.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import QuestCard from './QuestCard.js'
-
 import './resources/css/QuestJournal.css';
 
 import degree from './resources/images/degree.png'
@@ -18,14 +16,77 @@ function QuestJournal() {
 
             <div className="row">
                 <div className="col-lg-4">
-                    <QuestCard cardClass="quest-pink" title='TRAINING QUESTS' questImg={degree} imgClass="quest-type-white"/>
+                    <QuestCard cardClass="card quest-card card-pink"
+                               title='TRAINING QUESTS'
+                               questImg={degree}
+                               imgClass="quest-type-white"
+                    />
+                    <ProjectCard/>
                 </div>
                 <div className="col-lg-4">
-                    <QuestCard cardClass="quest-orange" title="MAIN QUESTS" questImg={tie} imgClass="quest-type-black"/>
+                    <QuestCard
+                        cardClass="card quest-card card-orange"
+                        title="MAIN QUESTS"
+                        questImg={tie}
+                        imgClass="quest-type-black"
+                    />
+                    <ProjectCard/>
                 </div>
                 <div className="col-lg-4">
-                    <QuestCard cardClass="quest-pink" title="SIDE QUESTS" questImg={pencilRuler} imgClass="quest-type-white"/>
+                    <QuestCard
+                        cardClass="card quest-card card-pink"
+                        title="SIDE QUESTS"
+                        questImg={pencilRuler}
+                        imgClass="quest-type-white"
+                    />
+                    <ProjectCard/>
                 </div>
+            </div>
+        </div>
+    );
+}
+
+function QuestCard (props) {
+
+    function trainingTransition() {
+        let questCards = document.querySelectorAll(".quest-card");
+        for (let i = 0; i < questCards.length; ++i) {
+            questCards[i].classList.toggle("hide")
+        }
+
+        let questWrapper = document.querySelectorAll(".quest-wrapper");
+        for (let i = 0; i < questWrapper.length; ++i) {
+            questWrapper[i].classList.toggle("hide")
+        }
+
+        let trainingCards = document.querySelectorAll(".training-card");
+        for (let i = 0; i < trainingCards.length; ++i) {
+            trainingCards[i].classList.toggle("show")
+        }
+
+        let trainingWrapper = document.querySelectorAll(".training-wrapper");
+        for (let i = 0; i < trainingWrapper.length; ++i) {
+            trainingWrapper[i].classList.toggle("show")
+        }
+    }
+
+    return (
+        <div className="quest-wrapper">
+            <div className={props.cardClass} onClick={trainingTransition}>
+                <p className="card-title">{props.title}</p>
+                <img src={props.questImg} alt="questImg" className={props.imgClass}/>
+            </div>
+        </div>
+    );
+}
+
+function ProjectCard (props) {
+
+    return (
+        <div className="training-wrapper">
+            <div className="card training-card card-pink">
+                <p className="card-title">MCMASTER UNIVERSITY</p>
+                <img src={tie} alt="mcmaster"/>
             </div>
         </div>
     );
